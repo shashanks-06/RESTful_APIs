c3523719960129f1cc1c4dffcaee3c9a41bac684
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-12 17:30:44 +0530

    done serialization

--- 1.Introduction_to_RESTful_APIs/2.Serialization/app.js
+++ 1.Introduction_to_RESTful_APIs/2.Serialization/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ foo: "bar" }); // Express Serialize object Data to JSON
+  res.send({ foo: "bar" }); // Express ("send" done Serialization)  Serialize object Data to JSON
 });
 
 app.listen(3000, console.log("Listening on port 3000..."));
--- 1.Introduction_to_RESTful_APIs/2.Serialization/index.js
+++ 1.Introduction_to_RESTful_APIs/2.Serialization/index.js
@@ -2,7 +2,13 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write({ foo: "bar" }); // Can't Serialize object Data to JSON
+    // res.write({ foo: "bar" }); // Can't Serialize object Data to JSON
+
+    res.write(JSON.stringify({ foo: "bar" })); //Serialize object Data to JSON
+
+    console.log(JSON.parse('{"foo": "bar"}')); //Deserialize JSON to object
+    //Reload page and you'll get output from server
+
     res.end();
   }
 });
