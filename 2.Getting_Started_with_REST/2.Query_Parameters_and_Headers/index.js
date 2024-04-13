bba95f495382703dcb0af71e5aa1a2ca2ae0c7fb
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-13 17:58:31 +0530

    querying parameters and headers

--- 2.Getting_Started_with_REST/2.Query_Parameters_and_Headers/index.js
+++ 2.Getting_Started_with_REST/2.Query_Parameters_and_Headers/index.js
@@ -4,12 +4,35 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 5000;
 
+let notes = [
+  {
+    title: "Sample Note",
+    content: "A note should have a content",
+    isDraft: true,
+  },
+  {
+    title: "Sample Note 2",
+    content: "A content should have a Note",
+    isDraft: true,
+  },
+];
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello from Noter"));
 
 app.get("/notes", (req, res) => {
-  res.send("All Notes");
+  res.send(notes);
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  res.send("Created a note");
+});
+
+app.get("/note/:id", (req, res) => {
+  console.log(req.params);
+  res.send("A Note");
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
