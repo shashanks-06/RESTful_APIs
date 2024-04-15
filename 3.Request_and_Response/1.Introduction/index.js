3b6a92feb0bede0f31b0ec3197d537e526abdde4
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-15 12:10:36 +0530

    request and response - intro

--- 3.Request_and_Response/1.Introduction/index.js
+++ 3.Request_and_Response/1.Introduction/index.js
@@ -1,7 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {
+  getNotes,
+  getNote,
+  createNote,
+  updateNote,
+  deleteNote,
+} from "./controllers/v1/note";
 
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, console.log(`Server listening on PORT: ${PORT}`));
+app.get("/v1/notes", getNotes);
+app.get("/v1/notes/:id", getNote);
+app.post("/v1/notes", createNote);
+app.delete("/v1/notes/:id", deleteNote);
+app.patch("/v1/notes/:id", updateNote);
+
+app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
