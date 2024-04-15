46075251a743d4ec1f3962ed8891584f2000975d
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-15 13:02:40 +0530

    controllers and actions

---
+++ 3.Request_and_Response/2.Controllers_and_Actions/controllers/v1/notes.js
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+
+let notes = [];
+
+export const getNotes = (req, res) => {
+  res.send(notes);
+};
+
+export const createNote = (req, res) => {
+  const note = req.body;
+  notes.push({ ...note, id: uuidv4() });
+  res.send(`Added note with title: ${note.title}.`);
+};
+
+export const getNote = (req, res) => {
+  const { id } = req.params;
+
+  const note = notes.find((note) => note.id === id);
+
+  res.send(note);
+};
+
+export const deleteNote = (req, res) => {
+  const { id } = req.params;
+
+  notes = notes.filter((note) => note.id !== id);
+
+  res.send(`Note with id: ${id} is deleted`);
+};
+
+export const updateNote = (req, res) => {
+  const { id } = req.params;
+  const { title, content, isDraft } = req.body;
+
+  const note = notes.find((note) => note.id === id);
+
+  if (title) {
+    note.title = title;
+  }
+
+  if (content) {
+    note.content = content;
+  }
+  if (isDraft) {
+    note.isDraft = isDraft;
+  }
+
+  res.send(`A note with id: ${id} is updated`);
+};
--- 3.Request_and_Response/2.Controllers_and_Actions/index.js
+++ 3.Request_and_Response/2.Controllers_and_Actions/index.js
@@ -7,9 +7,11 @@ import {
   updateNote,
   deleteNote,
 } from "./controllers/v1/note.js";
+import { getNotes } from "./controllers/v2/notes.js";
 
 const app = express();
 const PORT = 5000;
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello from Noter"));
@@ -20,4 +22,8 @@ app.post("/v1/notes", createNote);
 app.delete("/v1/notes/:id", deleteNote);
 app.patch("/v1/notes/:id", updateNote);
 
-app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
+app.get("/v2/notes", getNotes);
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
