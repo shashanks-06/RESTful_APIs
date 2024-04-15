f89375f9557318cf10f6d55f1adfc17c4d30b1f3
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-15 17:09:09 +0530

    model validation

--- 3.Request_and_Response/4.Model_Validation/controllers/v1/notes.js
+++ 3.Request_and_Response/4.Model_Validation/controllers/v1/notes.js
@@ -1,4 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
+import validator from "express-validator";
+
+const { validationResult } = validator;
 
 let notes = [];
 
@@ -7,6 +10,12 @@ export const getNotes = (req, res) => {
 };
 
 export const createNote = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  console.log(errors);
+
   const note = req.body;
   notes.push({ ...note, id: uuidv4() });
   res.send(`Added note with title: ${note.title}.`);
--- 3.Request_and_Response/4.Model_Validation/routes/v1/node.js
+++ 3.Request_and_Response/4.Model_Validation/routes/v1/node.js
@@ -1,4 +1,6 @@
 import express from "express";
+import validator from "express-validator";
+
 import {
   getNotes,
   getNote,
@@ -9,10 +11,21 @@ import {
 
 const router = express.Router();
 
+const { body } = validator;
+
 router.get("/", getNotes);
+
 router.get("/:id", getNote);
-router.post("/", createNote);
+
+router.post(
+  "/",
+  body("title").exists(),
+  body("isDraft").isBoolean(),
+  createNote
+);
+
 router.delete("/:id", deleteNote);
+
 router.patch("/:id", updateNote);
 
 export default router;
