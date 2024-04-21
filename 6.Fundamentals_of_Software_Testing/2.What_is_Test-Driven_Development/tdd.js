3ade7f862a22674b58345cc84105af297ff42448
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-21 16:33:57 +0530

    Test-Driven Development

--- 6.Fundamentals_of_Software_Testing/2.What_is_Test-Driven_Development/tdd.js
+++ 6.Fundamentals_of_Software_Testing/2.What_is_Test-Driven_Development/tdd.js
@@ -8,7 +8,12 @@ function expect(actual) {
   };
 }
 expect(add(4, 3)).toBe(7);
+expect(add(1, 2, 3)).toBe(6);
 
-function add(a, b) {
-  return a + b;
+function add(...numbers) {
+  let sum = 0;
+  for (let num of numbers) {
+    sum += num;
+  }
+  return sum;
 }
