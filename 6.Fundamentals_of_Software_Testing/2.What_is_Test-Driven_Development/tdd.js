7a56ff98f7a056bd26f624630a4176cf68422b8a
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-21 16:27:10 +0530

    Test-Driven Development

---
+++ 6.Fundamentals_of_Software_Testing/2.What_is_Test-Driven_Development/tdd.js
@@ -0,0 +1,9 @@
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+  };
+}
