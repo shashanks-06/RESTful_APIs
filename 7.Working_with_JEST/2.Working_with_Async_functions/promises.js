3b2ea54d8a5290414de691f112df55d3746bd01e
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-23 17:49:51 +0530

    Async Functions

--- 7.Working_with_JEST/2.Working_with_Async_functions/promises.js
+++ 7.Working_with_JEST/2.Working_with_Async_functions/promises.js
@@ -5,3 +5,21 @@ test("the data is cereal", () => {
     expect(data).toBe("cereal");
   });
 });
+
+/* -------------------------------------------------------------------------------------------- */
+
+// 2. Async/Await
+
+test("the data is cereal", async () => {
+  const data = await fetchData();
+  expect(data).toBe("cereal");
+});
+
+test("the fetch fails with an error", async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (err) {
+    expect(err).toMatch("error");
+  }
+});
