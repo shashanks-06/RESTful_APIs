7183aa117072e97c54242f48f48c5721fbd4d191
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-23 16:08:34 +0530

    Matchers and configuration

--- 7.Working_with_JEST/1.Matchers_and_Configuration/matchers.js
+++ 7.Working_with_JEST/1.Matchers_and_Configuration/matchers.js
@@ -17,3 +17,23 @@ test("adding positive number is not zero", () => {
     }
   }
 });
+
+//Truthyness
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
