5c7d4b0ad17ca20f3f00baab229b7a7187fee538
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-24 17:05:36 +0530

    mock functions

--- 7.Working_with_JEST/4.Mocking_Part_1-Mock_Functions/mock.test.js
+++ 7.Working_with_JEST/4.Mocking_Part_1-Mock_Functions/mock.test.js
@@ -17,3 +17,12 @@ expect(mockCallback.mock.results[1].value).toBe(43);
 
 /*----------------------------------------------------------------------------------------------*/
 
+const myMock1 = jest.fn();
+const a = new myMock1();
+console.log(myMock1.mock.instances);
+
+const myMock2 = jest.fn();
+const b = {};
+const bound = myMock2.bind(b);
+bound();
+console.log(myMock2.mock.contexts);
