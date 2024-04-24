449ed0a553dd7c603a0fbcf35c8df6b914befae4
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-24 23:37:18 +0530

    jest unit testing

--- 7.Working_with_JEST/5.Unit_Testing_Project-Jest/__tests__/user.test.js
+++ 7.Working_with_JEST/5.Unit_Testing_Project-Jest/__tests__/user.test.js
@@ -13,3 +13,16 @@ it("works with resolve", () => {
   expect.assertions(1);
   return expect(user.getUserName(5)).resolves.toEqual("Paul");
 });
+
+// async/await can be used.
+it("works with async/await", async () => {
+  expect.assertions(1);
+  const data = await user.getUserName(4);
+  expect(data).toEqual("Mark");
+});
+
+// async/await can also be used with `.resolves`
+it("works with async/await and resolves", async () => {
+  expect.assertions(1);
+  await expect(user.getUserName(5)).resolves.toEqual("Paul");
+});
