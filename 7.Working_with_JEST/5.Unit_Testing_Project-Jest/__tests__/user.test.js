18c95b5e281d3e77db99e44643167dd5e20b426d
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-24 23:31:47 +0530

    jest unit testing

--- 7.Working_with_JEST/5.Unit_Testing_Project-Jest/__tests__/user.test.js
+++ 7.Working_with_JEST/5.Unit_Testing_Project-Jest/__tests__/user.test.js
@@ -8,3 +8,8 @@ it("works with promises", () => {
   expect.assertions(1);
   return user.getUserName(4).then((data) => expect(data).toEqual("Mark"));
 });
+
+it("works with resolve", () => {
+  expect.assertions(1);
+  return expect(user.getUserName(5)).resolves.toEqual("Paul");
+});
