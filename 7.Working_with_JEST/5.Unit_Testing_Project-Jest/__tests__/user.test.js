f14cbd0a2063c39808977234f3c8019feb8cbfb6
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-24 23:28:34 +0530

    jest unit testing

---
+++ 7.Working_with_JEST/5.Unit_Testing_Project-Jest/__tests__/user.test.js
@@ -0,0 +1,10 @@
+jest.mock("../request");
+
+import * as user from "../user";
+
+//The assertions for promise must be returned.
+
+it("works with promises", () => {
+  expect(assertions(1));
+  return user.getUserName(4).then((data) => expect(data).toEqual("Mark"));
+});
