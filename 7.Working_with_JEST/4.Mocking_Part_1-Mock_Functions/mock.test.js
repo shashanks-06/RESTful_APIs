e024117caffb6939c2afb36c1fe4a71015e9a1d2
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-04-24 17:21:35 +0530

    mock functions

--- 7.Working_with_JEST/4.Mocking_Part_1-Mock_Functions/mock.test.js
+++ 7.Working_with_JEST/4.Mocking_Part_1-Mock_Functions/mock.test.js
@@ -55,3 +55,16 @@ myMock
   .mockReturnValueOnce(true);
 console.log(myMock(), myMock(), myMock(), myMock());
 // >            10      'x'       true      true
+
+/*----------------------------------------------------------------------------------------------*/
+
+const filterTestFn = jest.fn();
+
+filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+
+const result = [11, 12].filter((num) => filterTestFn(num));
+
+console.log(result); // 11
+
+console.log(filterTestFn.mock.calls[0][0]); // 11
+console.log(filterTestFn.mock.calls[1][0]); // 12
