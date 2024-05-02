c509e5438a329500ba2c3dd8f549877630f561ea
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-02 15:12:21 +0530

    vitest coverage testing

--- 8.Introduction_to_Vitest/2.Coverage_Reports_Concurrent_Tests_and_Filtering/src/sum.js
+++ 8.Introduction_to_Vitest/2.Coverage_Reports_Concurrent_Tests_and_Filtering/src/sum.js
@@ -9,15 +9,15 @@ export default function sum(...numbers) {
 
 if (import.meta.vitest) {
   describe("#sum", () => {
-    it("2+3 = 5", () => {
+    it.concurrent("2+3 = 5", () => {
       expect(sum(2, 3)).toBe(5);
     });
 
-    it("Returns same no. with one no.", () => {
+    it.only("Returns same no. with one no.", () => {
       expect(sum(3)).toBe(3);
     });
 
-    it("Returns sum with multiple no.", () => {
+    it.skip("Returns sum with multiple no.", () => {
       expect(sum(5, 10, 15)).toBe(30);
     });
   });
