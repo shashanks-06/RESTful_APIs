30fe6a4dfb2fa2201199c2b7f859e7bcd5bc1357
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-03 16:30:11 +0530

    Headless PDF done

--- 9.E2E_with_Puppeteer/1.Browser_operations_and_basic_commands/example.js
+++ 9.E2E_with_Puppeteer/1.Browser_operations_and_basic_commands/example.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 
+// Screenshot of website
 async function screenshot() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -8,5 +9,15 @@ async function screenshot() {
 
   await browser.close();
 }
-
 screenshot();
+
+// PDF of website
+async function pdf() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://knowledgehut.com", { waitUntil: "networkidle2" }); // wait until website is loaded completely
+  await page.pdf({ path: "kh.pdf", format: "a4" }); // page formate set to a4 size
+
+  await browser.close();
+}
+pdf();
