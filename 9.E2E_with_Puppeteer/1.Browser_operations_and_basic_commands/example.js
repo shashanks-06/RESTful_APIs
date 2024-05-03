6a2eaaf50c00d0a36fb6ca92aec1584451223c29
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-05-03 16:22:20 +0530

    Headless Screenshot

--- 9.E2E_with_Puppeteer/1.Browser_operations_and_basic_commands/example.js
+++ 9.E2E_with_Puppeteer/1.Browser_operations_and_basic_commands/example.js
@@ -1,1 +1,12 @@
+const puppeteer = require("puppeteer");
 
+async function screenshot() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://knowledgehut.com");
+  await page.screenshot({ path: "kh.png" });
+
+  await browser.close();
+}
+
+screenshot();
